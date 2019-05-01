c8b20ed1dbdede3f63b1ca60711483a1cb8c1a9e
Allison Gu <allisonjgu420@gmail.com>
2019-05-01 09:32:57 -0700

    almost mvp

--- BigfootSmallfoot/app/src/main/java/com/example/allison/bigfootsmallfoot/MainActivity.java
+++ BigfootSmallfoot/app/src/main/java/com/example/allison/bigfootsmallfoot/MainActivity.java
@@ -16,18 +16,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button loginButton = (Button) findViewById(R.id.loginButton);
+       /* Button loginButton = (Button) findViewById(R.id.loginButton);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, Dashboard.class));
             }
-        });
+        });*/
     }
 
+
+    /*DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+DatabaseReference animalsRef = rootRef.child("Animals");
+ValueEventListener eventListener = new ValueEventListener() {
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
+        Boolean found;
+        String search = "Animals";
+        for(DataSnapshot ds : dataSnapshot.getChildren()) {
+            String movieName = ds.child("movieName").getValue(String.class);
+            found = movieName.contains(search);
+            Log.d("TAG", movieName + " / " + found);
+        }
+    }
+
+    @Override
+    public void onCancelled(DatabaseError databaseError) {}
+};
+animalsRef.addListenerForSingleValueEvent(eventListener);
+
+*/
+
+   /* public void onLoginClick (View v) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef1 = database.getReference("/users/test");
+
+        if (1 == 1) {
+            myRef1.
+
+        } else {
+            startActivity(new Intent(MainActivity.this, Dashboard.class));
+        }
+    }
     public void onCreateAccountClick (View v) {
         startActivity(new Intent(MainActivity.this, CreateAccount.class));
     }
+    */
 
 }
